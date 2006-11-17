87fcc58fa4a6e3c0c65b23d6040deb44da90221f
"Shane Holloway" <none@none>
2006-11-16 17:12:37 -0700

    Refactored color module to always work in floating point.  Refactored flow a bit.

--- proj/openGL/color.py
+++ proj/openGL/color.py
@@ -24,48 +24,23 @@ class ColorObject(object):
     glColorFnMap = {None: glColorNoOp, 0: glColorNoOp, 3: gl.glColor3fv, 4: gl.glColor4fv}
     select = None # replaced with a fn from glColorFnMap to "select" the color
 
-    def __init__(self, value=None, dataFormat='f'):
+    def __init__(self, value=None, dataFormat=None):
         self.setValue(value, dataFormat)
 
     _value = None
     def getValue(self):
         return self._value
-    def setValue(self, value, dataFormat='f'):
+    def setValue(self, value, dataFormat=None):
         if value is None:
             glColorFn = self.glColorFnMap[None]
             self.select = glColorFn
             self._value = value
             return
-
-        if not isinstance(value, tuple):
-            if isinstance(value, (long, int)):
-                if dataFormat in ('h', 'hex', 'rgb'):
-                    value = (v/255. for v in ((value >> 16) & 0xff, (value >> 8) & 0xff, (value) & 0xff))
-                elif dataFormat in ('ha', 'hexalpha', 'hexa', 'rgba'):
-                    value = (v/255. for v in ((value >> 32) & 0xff, (value >> 16) & 0xff, (value >> 8) & 0xff, (value) & 0xff))
-                else: 
-                    raise ValueError("Do not know how to interpret %r as a color with format %r" % (value, dataFormat))
-            elif isinstance(value, float):
-                if dataFormat in ('l', 'L', 'rgb', 'RGB'):
-                    value = (value, value, value)
-                elif dataFormat in ('i', 'I', 'la', 'LA', 'rgba', 'RGBA', 'a', 'A'):
-                    value = (value, value, value, value)
-                else: 
-                    raise ValueError("Do not know how to interpret %r as a color with format %r" % (value, dataFormat))
-            elif isinstance(value, basestring):
-                if value.startswith('#'):
-                    value = value[1:]
-                elif value.startswith('0x'):
-                    value = value[2:]
-                else: 
-                    raise ValueError("Do not know how to interpret %r as a color with format %r" % (value, dataFormat))
-
-                if len(value) <= 4:
-                    # 3 or 4 length hex string
-                    value = tuple(int(e, 16)/15. for e in (value[0:1], value[1:2], value[2:3], value[3:]) if e)
-                else:
-                    # 6 or 8 length hex string
-                    value = tuple(int(e, 16)/255. for e in (value[0:2], value[2:4], value[4:6], value[6:]) if e)
+        elif isinstance(value, tuple):
+            if len(value) == 2:
+                value = self.convertValue(*value)
+        else:
+            value = self.convertValue(value, dataFormat)
 
         glColorFn = self.glColorFnMap[len(value)]
         value = asarray(value, float32)
@@ -74,6 +49,45 @@ class ColorObject(object):
     def delValue(self):
         self.setValue(None)
     value = property(getValue, setValue, delValue)
+
+    def convertValue(self, value, dataFormat):
+        if isinstance(value, (long, int)):
+            if dataFormat in ('h', 'hex', 'rgb'):
+                value = (v/255. for v in ((value >> 16) & 0xff, (value >> 8) & 0xff, (value) & 0xff))
+            elif dataFormat in ('ha', 'hexalpha', 'hexa', 'rgba'):
+                value = (v/255. for v in ((value >> 32) & 0xff, (value >> 16) & 0xff, (value >> 8) & 0xff, (value) & 0xff))
+            else: 
+                if value > 255:
+                    raise ValueError("Do not know how to interpret %r as a color with format %r" % (value, dataFormat))
+                if value > 1:
+                    value /= 255.
+                value = (value, value, value)
+
+        elif isinstance(value, float):
+            if dataFormat in ('i', 'I', 'la', 'LA', 'rgba', 'RGBA', 'a', 'A'):
+                value = (value, value, value, value)
+            elif dataFormat in ('l', 'L', 'rgb', 'RGB'):
+                value = (value, value, value)
+            else: 
+                value = (value, value, value)
+
+        elif isinstance(value, basestring):
+            value = value.replace(' ', '')
+            if value.startswith('#'):
+                value = value[1:]
+            elif value.startswith('0x'):
+                value = value[2:]
+
+            if len(value) > 4:
+                # 6 or 8 length hex string
+                value = tuple(int(e, 16)/255. for e in (value[0:2], value[2:4], value[4:6], value[6:]) if e)
+            elif len(value) > 3:
+                # 3 or 4 length hex string
+                value = tuple(int(e, 16)/15. for e in (value[0:1], value[1:2], value[2:3], value[3:]) if e)
+            else:
+                raise ValueError("Do not know how to interpret %r as a color with format %r" % (value, dataFormat))
+
+        return value
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
